02e1362d2b74efcd8807ece4351908f64f3c48a9
Prashoon123 <64588766+Prashoon123@users.noreply.github.com>
2020-08-30 22:51:46 +0530

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
     if (gameOver) {
         if (confirm('You lost. Press ok to restart.')) {
-            window.location.href = 'https://prashoon123.github.io/Snake-Game/'
+            window.location.href = 'https://prashoon123.github.io/SnakeGame/'
         }
         return
     }
